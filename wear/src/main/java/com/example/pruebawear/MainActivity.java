b61445c5e12d2bcbf377d20d48f7eb2160792e4d
Carlos Ariel SB <arielsb.ti19@utsjr.edu.mx>
2022-06-09 21:36:34 -0500

    Envio V.1.2

--- wear/src/main/java/com/example/pruebawear/MainActivity.java
+++ wear/src/main/java/com/example/pruebawear/MainActivity.java
@@ -1,15 +1,44 @@
 package com.example.pruebawear;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.style.UpdateAppearance;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.core.app.NotificationBuilderWithBuilderAccessor;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.example.pruebawear.databinding.ActivityMainBinding;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends Activity {
 
-    private TextView mTextView;
+    private Button wButton = null;
     private ActivityMainBinding binding;
+    private Intent intent;
+    private PendingIntent pendingIntent;
+
+    private NotificationCompat.Builder notification;
+    private NotificationManagerCompat nm;
+    private NotificationCompat.WearableExtender wearableExtender;
+
+    String idChannel = "Mi_Canal";
+    int idNotification = 001;
+
+    private NotificationCompat.BigTextStyle bigTextStyle;
+
+    String longText = "Without BigStyle, only a single line of text would be visible"+
+            "Any additional text would not appear directly in the notification";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +47,61 @@ public class MainActivity extends Activity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mTextView = binding.text;
+        wButton = findViewById(R.id.wButton);
+
+        intent = new Intent(MainActivity.this, MainActivity.class);
+
+        nm = NotificationManagerCompat.from(MainActivity.this);
+
+        wearableExtender = new NotificationCompat.WearableExtender();
+        bigTextStyle = new NotificationCompat.BigTextStyle().bigText(longText);
+
+        wButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                start(view);
+                int importance = NotificationManager.IMPORTANCE_HIGH;
+                String name = "Notification";
+                NotificationChannel notificationChannel = new NotificationChannel(idChannel, name, importance);
+
+                nm.createNotificationChannel(notificationChannel);
+
+                pendingIntent = PendingIntent.getActivity(MainActivity.this,0,intent,0);
+
+                notification = new NotificationCompat.Builder(MainActivity.this, idChannel)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Mi notification")
+                        .setContentText("Mi primera notificación")
+                        .extend(wearableExtender);
+                nm.notify(idNotification, notification.build());
+            }
+
+            public void start(View view){
+                Timer t = new Timer();
+                TimerTask tt = new TimerTask() {
+                    @Override
+                    public void run() {
+                        int importance = NotificationManager.IMPORTANCE_HIGH;
+                        String name = "Mi actualización";
+                        NotificationChannel notificationChannel = new NotificationChannel(idChannel, name, importance);
+
+                        nm.createNotificationChannel(notificationChannel);
+
+                        pendingIntent = PendingIntent.getActivity(MainActivity.this,0,intent,0);
+
+                        notification = new NotificationCompat.Builder(MainActivity.this, idChannel)
+                                .setSmallIcon(R.mipmap.ic_launcher)
+                                .setContentTitle("Mi notification")
+                                .setContentText("Notificacion actualizada")
+                                .extend(wearableExtender);
+                        nm.notify(idNotification, notification.build());
+                    }
+                };
+
+                t.schedule(tt, 5000);
+
+            }
+        });
     }
 }
